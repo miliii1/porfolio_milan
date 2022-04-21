df2c7ab6c367a5760698eede877d909ebc7292a8
miliii1 <milagrosjara26@gmail.com>
2022-04-21 20:38:35 -0300

    problemas con skills

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+/* ========================== SHOW MENU ====================================== */
+//<i class="lni lni-download"></i>
+/* ============== MENU SHOW ============= */
+
+/* ============== MENU HIDDEN ============= */
+
+/* ============== REMOVE MENU MOBILE ============= */
+
+/* ============== CHANGE BACKGROUND HEADER ============= */
+
+/* ============== SCROLL SECTIONS ACTIVE LINK ============= */
+
+/* ============== PORFOLIO ITEM FILTER ============= */
+
+/* ============== THEME/DISPLAY CUSTOMIZATION ============= */
+
+/* ============== FONT ============= */
+
+/* ============== PRIMARY COLORS ============= */
+
+/* ============== THEME BACKGROUNDS ============= */
